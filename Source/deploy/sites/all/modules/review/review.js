e01e9862c551744b1166043d3cadb5b3446dcb70
Shulabha <shulabha.kumari@pfizer.com>
2017-09-14 20:42:14 +0530

    add

--- Source/deploy/sites/all/modules/review/review.js
+++ Source/deploy/sites/all/modules/review/review.js
@@ -2,14 +2,14 @@
   $(document).ready(function() {
 	$('#block-review-review-select-form').magnificPopup({
 		type: 'inline',
-		preloader: false,
-		focus: '#name',
+		//preloader: false,
+		//focus: '#name',
 
 		// When elemened is focused, some mobile browsers in some cases zoom in
 		// It looks not nice, so we disable it:
 		callbacks: {
 			beforeOpen: function() {
-				if(jQuery(window).width() < 700) {
+				if($(window).width() < 700) {
 					this.st.focus = false;
 				} else {
 					this.st.focus = '#name';
